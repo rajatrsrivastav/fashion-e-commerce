e92607c5b3f8a1e6d4f3c136baafa41059faa5f4
Rajat Srivastav <ratneshrsrivastav@gmail.com>
2026-02-04 15:57:32 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -14,18 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: true,
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   exposedHeaders: ['ETag', 'Last-Modified']
-}));
-
-app.options('*', cors({
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
 app.use(express.json());
@@ -51,7 +44,7 @@ app.use('/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'E-commerce API is running!'
+    message: 'Fashion E-commerce API is running!'
   });
 });
 
